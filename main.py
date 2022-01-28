0dbf6e53f0b28f0b0220b05fd008506d72577fa5
Topabaem05 <choijjs83@gmail.com>
2022-01-28 17:27:25 +0900

    xx\

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template,request,redirect
-from scrapper import get_jobs
-app=Flask("SuperScapper")
+from flask import Flask, render_template
+
+app = Flask("Flask")
+
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/report")
 def report():
- word=request.args.get('word')
- if word:
-  word=word.lower()
-  jobs=get_jobs(word)
- else:
-   return redirect("/") 
- return render_template("report.html")
- 
-app.run(host="0.0.0.0") 
+    return "This is the report"
+
+
+app.run(host="0.0.0.0")
