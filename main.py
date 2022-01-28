2fe3c6c449c7160ed9a1af6a74a265fa0af81d4c
Topabaem05 <choijjs83@gmail.com>
2022-01-28 09:22:46 +0900

    rtyui

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from re import search
-from main import Flask, render_template, request
-
-app = Flask("web")
+from flask import Flask, render_template,request,redirect
+from scrapper import get_jobs
+app=Flask("SuperScapper") #앱 이름 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+  return render_template("potato.html")
 
 @app.route("/report")
 def report():
-    word = request.arg.get('word')
-    return render_template("report.html",word=word)
+ word=request.args.get('word')
+ if word:
+  word=word.lower()
+  jobs=get_jobs(word)
+ else:
+   return redirect("/") 
+ return render_template("report.html",searchingBy=word)
 
-app.run(host="0.0.0.0")
+app.run(host="0.0.0.0") 
